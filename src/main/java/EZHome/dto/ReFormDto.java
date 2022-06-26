c25e00755b6a472cef88e2d987d481d469930780
sohnr <64889722+sohnr@users.noreply.github.com>
2022-06-27 04:27:54 +0900

    매물 업로드 작업중 01
    매물 업로드 작업중 01

--- src/main/java/EZHome/dto/ReFormDto.java
+++ src/main/java/EZHome/dto/ReFormDto.java
@@ -11,35 +11,68 @@ import javax.validation.constraints.NotBlank;
 public class ReFormDto {
     private int reid ; // 매물 기본키 (fk)
 
-    @NotBlank(message="매물 유형은 필수 입력 값입니다.")
-    private String rehouseType ; // 매물 유형
-
     @NotBlank(message="매물 번호는 필수 입력 값입니다.")
     private int reNum;  // 매물 번호
 
-    @NotBlank(message="'시도' 작성은 필수 입력 값입니다.")
+    @NotBlank(message="매물 유형은 필수 입력 값입니다.")
+    private String rehouseType ; // 매물 유형
+
+    @NotBlank(message="매매 유형은 필수 입력 값입니다.")
+    private String retrType ; // 매매 유형
+
+    @NotBlank(message="월세는 필수 입력 값입니다.")
+    private int reMon_price ; // 월세
+
+    @NotBlank(message="관리비는 필수 입력 값입니다.")
+    private int reAdmn_fee ; // 관리비
+
+    @NotBlank(message="보증금는 필수 입력 값입니다.")
+    private int reDeposit; // 보증금
+
+    @NotBlank(message="매매가는 필수 입력 값입니다.")
+    private int reTrade ; // 매매가
+
+    @NotBlank(message="전세는 필수 입력 값입니다.")
+    private int reJeonse ; // 전세
+
+    @NotBlank(message="'시도' 작성은 필수 입력입니다.")
     private String reSido; // 시도
 
-    @NotBlank(message="'군구' 작성은 필수 입력 값입니다.")
+    @NotBlank(message="'군구' 작성은 필수 입력입니다.")
     private String reGungu; // 군구
 
-    @NotBlank(message="'동' 작성은 필수 입력 값입니다.")
+    @NotBlank(message="'동' 작성은 필수 입력입니다.")
     private String reDong; // 동
 
-    @NotBlank(message="'상세주소' 작성은 필수 입력 값입니다.")
+    @NotBlank(message="'상세주소' 작성은 필수 입력입니다.")
     private String reDtl_Adr ; // 상세주소
 
-
+    @NotBlank(message="방개수는 필수 입력입니다.")
     private int reRoomcnt ; //방개수
+
+    @NotBlank(message="욕실개수는 필수 입력 값입니다.")
     private int reBathcnt; // 욕실개수
+
+    @NotBlank(message="평수는 필수 입력 값입니다.")
+    private int reExtent ; // 평수
 
     @NotBlank(message="충수는 필수 입력 값입니다.")
     private int reFlr ; // 층수
 
+    @NotBlank(message="전체 층수는 필수 입력 값입니다.")
     private int reTotalFlr ; // 전체 층수
+
+
     private boolean reSecndFlr ; //  복층
+
+
     private boolean reTopFlr ; // 옥탑방
+
+
     private boolean reUndrflr ; // 반지하
+
+
+
 
 
     //상품에 대한 이미지 정보를 저장하고 있는 List 컬렉션입니다.
--- src/main/java/EZHome/entity/ReEs.java
+++ src/main/java/EZHome/entity/ReEs.java
@@ -1,5 +1,6 @@
 package EZHome.entity;
 
+import EZHome.dto.ReFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,6 +11,41 @@ import javax.persistence.*;
 @Table(name="ReEs") // 매물
 @Getter @Setter @ToString
 public class ReEs {
+
+
+    // 매물에 대한 정보를 업데이트하는 비즈니스 로직을 구현합니다.
+    public void updateItem(ReFormDto reFormDto){
+        this.reNum = reFormDto.getReNum(); // 매물번호
+        this.rehouseType = reFormDto.getRehouseType(); // 매물유형
+        this.retrType=reFormDto.getRetrType(); // 매매유형
+        this.reMon_price = reFormDto.getReMon_price(); // 월세
+        this.reAdmn_fee = reFormDto.getReAdmn_fee(); // 관리비
+        this.reDeposit = reFormDto.getReDeposit(); // 보증금
+        this.reTrade = reFormDto.getReTrade(); // 매매가
+        this.reJeonse = reFormDto.getReJeonse(); //전세
+
+
+        this.reSido = reFormDto.getReSido(); //시도
+        this.reGungu = reFormDto.getReGungu(); // 군구
+        this.reDong = reFormDto.getReDong(); // 동
+        this.reDtl_Adr = reFormDto.getReDtl_Adr(); //상세주소
+
+        this.reRoomcnt = reFormDto.getReRoomcnt(); // 방개수
+        this.reBathcnt = reFormDto.getReBathcnt(); // 화장실개수
+        this.reExtent = reFormDto.getReExtent(); // 평수
+
+        this.reFlr = reFormDto.getReFlr(); // 층수
+        this.reTotalFlr = reFormDto.getReTotalFlr(); // 전체층수
+
+
+        this.reSecndFlr = reFormDto.isReSecndFlr(); // 복층
+        this.reTopFlr = reFormDto.isReTopFlr(); // 옥탑방
+        this.reUndrflr = reFormDto.isReUndrflr();// 반지하
+
+    }
+
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int reid ; // 매물 기본키 (FK)
@@ -42,12 +78,24 @@ public class ReEs {
     private int reFlr ; // 층수
 
     /* 전체층수부터 평수는 빠른 구현을 위해 'null 값 허용' 으로 설정하겠습니다.*/
+
+    @Column(nullable = false)
     private int reTotalFlr ; // 전체 층수
+
+
     private boolean reSecndFlr ; //  복층
+
+
     private boolean reTopFlr ; // 옥탑방
+
+
     private boolean reUndrflr ; // 반지하
+
+    @Column(nullable = false)
     private int reRoomcnt ; //방개수
+    @Column(nullable = false)
     private int reBathcnt; // 욕실개수
+    @Column(nullable = false)
     private int reExtent ; // 평수
 
     @Column(nullable = false)
@@ -62,12 +110,14 @@ public class ReEs {
     @Column(nullable = false)
     private int reTrade ; // 매매가
 
+    @Column(nullable = false)
+    private int reJeonse ; // 전세가
+
     //회원(일)이 여러개(다)의 매물을 올릴 수 있음으로 '일대다'로 설정했습니다.
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
 
-    // 매물에 대한 정보를 업데이트하는 비즈니스 로직을 구현합니다.
 
 }
