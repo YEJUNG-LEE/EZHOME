11f94c06d986f08073ff0f9fe4099d216740b7df
FrogDeveloper <97085193+FrogDevelpoer@users.noreply.github.com>
2022-06-28 23:21:40 +0900

    종욱
    6/29 일정
    - DB에 데이터 저장 되는지 확인
    - 매물 디테일로 조회 되는지 확인

--- src/main/java/EZHome/dto/ReFormDto.java
+++ src/main/java/EZHome/dto/ReFormDto.java
@@ -14,7 +14,7 @@ public class ReFormDto {
     private Long reid ; // 매물 기본키 (fk)
 
     @NotBlank(message="매물 번호는 필수 입력 값입니다.")
-    private int reNum;  // 매물 번호
+    private Integer reNum;  // 매물 번호
 
     @NotBlank(message="매물 유형은 필수 입력 값입니다.")
     private String rehouseType ; // 매물 유형
@@ -23,19 +23,19 @@ public class ReFormDto {
     private String retrType ; // 매매 유형
 
     @NotBlank(message="월세는 필수 입력 값입니다.")
-    private int reMon_price ; // 월세
+    private Integer reMon_price ; // 월세
 
     @NotBlank(message="관리비는 필수 입력 값입니다.")
-    private int reAdmn_fee ; // 관리비
+    private Integer reAdmn_fee ; // 관리비
 
     @NotBlank(message="보증금는 필수 입력 값입니다.")
-    private int reDeposit; // 보증금
+    private Integer reDeposit; // 보증금
 
     @NotBlank(message="매매가는 필수 입력 값입니다.")
-    private int reTrade ; // 매매가
+    private Integer reTrade ; // 매매가
 
     @NotBlank(message="전세는 필수 입력 값입니다.")
-    private int reJeonse ; // 전세
+    private Integer reJeonse ; // 전세
 
     @NotBlank(message="'시도' 작성은 필수 입력입니다.")
     private String reSido; // 시도
@@ -50,19 +50,19 @@ public class ReFormDto {
     private String reDtl_Adr ; // 상세주소
 
     @NotBlank(message="방개수는 필수 입력입니다.")
-    private int reRoomcnt ; //방개수
+    private Integer reRoomcnt ; //방개수
 
     @NotBlank(message="욕실개수는 필수 입력 값입니다.")
-    private int reBathcnt; // 욕실개수
+    private Integer reBathcnt; // 욕실개수
 
     @NotBlank(message="평수는 필수 입력 값입니다.")
-    private int reExtent ; // 평수
+    private Integer reExtent ; // 평수
 
     @NotBlank(message="충수는 필수 입력 값입니다.")
-    private int reFlr ; // 층수
+    private Integer reFlr ; // 층수
 
     @NotBlank(message="전체 층수는 필수 입력 값입니다.")
-    private int reTotalFlr ; // 전체 층수
+    private Integer reTotalFlr ; // 전체 층수
 
 
     private boolean reSecndFlr ; //  복층
