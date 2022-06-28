60e8f075b8162b947f7472b9dd15d1de398196ac
sohnr <64889722+sohnr@users.noreply.github.com>
2022-06-28 16:39:28 +0900

    소현
    소현

--- src/main/java/EZHome/controller/ReEsController.java
+++ src/main/java/EZHome/controller/ReEsController.java
@@ -20,14 +20,14 @@ import java.util.List;
 public class ReEsController {
 
     // 메물 올리기
-    @GetMapping(value="/admin/item/new")
+    @GetMapping(value="/item/new")
     public String reesInsert(Model model){
         model.addAttribute("ReFormDto", new ReFormDto());
         return "reEs/html/ReItemForm";
     }
 
     private final ReService reService;
-    @PostMapping(value = "/admin/item/new")
+    @PostMapping(value = "/item/new")
     public String itemNew(@Valid ReFormDto reFormDto, BindingResult bindingResult, Model model,
                           @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){
 
