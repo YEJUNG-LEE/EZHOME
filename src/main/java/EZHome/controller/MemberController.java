77e4b4bb422f3ebddee3f8b4cf4abbd82afb5b1e
daahani <102335953+daahani@users.noreply.github.com>
2022-06-29 10:09:22 +0900

    로그인 네비 작업중
    로그인 네비

---
+++ src/main/java/EZHome/controller/MemberController.java
@@ -0,0 +1,76 @@
+package EZHome.controller;
+
+import EZHome.constant.Role;
+import EZHome.dto.MemberFormDto;
+import EZHome.entity.Member;
+import EZHome.repository.MemberRepository;
+import EZHome.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value = "/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @ModelAttribute("role")
+    public Role[] role() {
+        return Role.values();
+    }
+
+    @GetMapping(value = "/login")
+    public String loginMember(){
+        return "member/memberLoginForm" ;
+    }
+
+    @GetMapping(value = "/login/error")
+    public String loginError(Model model){
+        model.addAttribute("loginErrorMsg", "이메일 또는 비밀번호를 확인해 주세요.") ;
+        return "member/memberLoginForm" ;
+    }
+//
+//    @GetMapping(value = "/error")
+//    public String loginError(){
+//        return "redirect:/login/error" ;
+//    }
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    private final MemberService memberService ;
+    private final PasswordEncoder passwordEncoder ;
+
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto,
+                            BindingResult bindingResult,
+                            Model model){
+        if(bindingResult.hasErrors()){
+//            System.out.println("요호서안 걸림");
+//            System.out.println(memberFormDto.toString());
+            return "member/memberForm" ;
+        }
+//        System.out.println("요호서안 안걸림");
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member) ;
+            return "redirect:/" ; // 메인 페이지로 이동
+        }catch (IllegalStateException e){
+            return "member/memberForm" ;
+        }
+    }
+}
