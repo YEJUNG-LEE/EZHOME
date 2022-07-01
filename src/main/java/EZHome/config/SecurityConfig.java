7fc382be2379548f19d410935cec97ed5c0f22c3
daahani <102335953+daahani@users.noreply.github.com>
2022-07-01 18:23:37 +0900

    부동산 네비
    부동산 탭 조정 , 네비 회원정보 작업

--- src/main/java/EZHome/config/SecurityConfig.java
+++ src/main/java/EZHome/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                .logoutSuccessUrl("/") ;
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true);
 
 
 
--- src/main/java/EZHome/controller/MainTestController.java
+++ src/main/java/EZHome/controller/MainTestController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @Controller
@@ -15,12 +16,11 @@ public class MainTestController {
     private final MemberService memberService ;
 
     @GetMapping(value = "/")
-    public String main(Principal principal, Model model){
+    public String main(Principal principal, Model model, HttpSession httpSession){
         if (principal != null) {
             String email = principal.getName() ;
             String username = memberService.userInfo(email) ;
-
-            model.addAttribute("username", username ) ;
+            httpSession.setAttribute("username", username);
         }
         return "common/main_content";
     }
