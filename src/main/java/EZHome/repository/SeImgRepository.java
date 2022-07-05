960bdb0d62b3597bfc6f4ada7c448c7b1b3c283e
FrogDeveloper <97085193+FrogDevelpoer@users.noreply.github.com>
2022-07-05 22:51:08 +0900

    07/05
    고객센터 1:1문의 디비 추가 과정 코딩

---
+++ src/main/java/EZHome/repository/SeImgRepository.java
@@ -0,0 +1,9 @@
+package EZHome.repository;
+
+import EZHome.entity.SeImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SeImgRepository extends JpaRepository<SeImg, Long> {
+}
