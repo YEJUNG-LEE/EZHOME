f435cfa0e50ec4f20eedad11b11608361a20e70b
FrogDeveloper <97085193+FrogDevelpoer@users.noreply.github.com>
2022-07-06 00:55:44 +0900

    07/06 12:55 종욱
    1. 헤더 고객센터 매핑 추가
    2. 고객센터 1:1 문의 html 및 css 소소한 변경

--- src/main/java/EZHome/repository/SeImgRepository.java
+++ src/main/java/EZHome/repository/SeImgRepository.java
@@ -1,9 +1,9 @@
-package EZHome.repository;
-
-import EZHome.entity.SeImg;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface SeImgRepository extends JpaRepository<SeImg, Long> {
-}
+//package EZHome.repository;
+//
+//import EZHome.entity.SeImg;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface SeImgRepository extends JpaRepository<SeImg, Long> {
+//}
