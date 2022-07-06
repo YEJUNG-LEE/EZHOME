583716ec60dd794d20ee7668b355b1c2fa5a07f9
FrogDeveloper <97085193+FrogDevelpoer@users.noreply.github.com>
2022-07-06 12:35:35 +0900

    07/06

--- src/main/java/EZHome/controller/ServiceController.java
+++ src/main/java/EZHome/controller/ServiceController.java
@@ -1,19 +1,10 @@
 // 고객센터 완성
 package EZHome.controller;
 
-import EZHome.dto.SeFormDto;
-import EZHome.service.SeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,7 +22,6 @@ public class ServiceController {
 
     @GetMapping(value = "service/oto")
     public String services_otoInsert(Model model){
-        model.addAttribute("seFormDto", new SeFormDto());
         return "services/SeCe-oto";
     }
 
--- src/main/java/EZHome/repository/SeImgRepository.java
+++
@@ -1,9 +0,0 @@
-//package EZHome.repository;
-//
-//import EZHome.entity.SeImg;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface SeImgRepository extends JpaRepository<SeImg, Long> {
-//}
