4417f5a63d871e7342ef79bd2ba6d7ed21c68929
daahani <102335953+daahani@users.noreply.github.com>
2022-07-06 18:03:18 +0900

    dahani
    디테일 및 다른거 수정 추가

--- src/main/resources/static/js/reEs/ReItemForm.js
+++ src/main/resources/static/js/reEs/ReItemForm.js
@@ -1,16 +1,6 @@
 /* ReItemForm.js */
 /* 맞춤, 카테고리 항목 클릭시 bg컬러 변경 */
 $(document).ready(function () {
-            var menu_offset = $('.main-menu').offset();  //offset() : 반환하는 객체는 left와 top 속성제공
-
-            $(window).scroll(function () {
-                if ($(window).scrollTop() > menu_offset.top) {
-                    $('.main-menu').addClass('menu-fixed');
-                } else {
-                    $('.main-menu').removeClass('menu-fixed');
-                }
-            });
-
             console.log(typeof($('input[value="true"]')));
             $('input[value="true"]').parent().css({'background-color':'#52b0fb', 'color':'white'});
 
